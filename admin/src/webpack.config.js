f44495837b8c0d7231cc7f26bf21a433811d9f71
yavis1218 <niko121879@gmail.com>
2023-11-09 11:25:42 +1000

    add webpack.config.js

---
+++ admin/src/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // other webpack options here...
+  devServer: {
+    allowedHosts: [
+      // Add your Ubuntu server's IP address or domain name here
+      '137.184.16.50'
+    ],
+  },
+};
